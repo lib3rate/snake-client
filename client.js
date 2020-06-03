b46097ee6af03718804250ce052994efef665d34
Dmitry Petrov <e.liberate@gmail.com>
2020-06-03 18:07:31 +0000

    Added a connect event listener

--- client.js
+++ client.js
@@ -1,14 +1,18 @@
 const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: "135.23.222.148",
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
   conn.on("data", data => {
     console.log(data)
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: DPE")
   });
   return conn;
 }
