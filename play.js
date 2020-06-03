7898cf9b3f246457470cf308afecc569cc9acf43
Dmitry Petrov <e.liberate@gmail.com>
2020-06-03 18:57:45 +0000

    Added standard input

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
-// play.js
 const { connect } = require('./client');
+
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+setupInput();
+
